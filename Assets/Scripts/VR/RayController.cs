31128d09b833bbff29d790c8544ac5e9b6746b52
hzfsls <2508342095@qq.com>
2021-08-24 14:57:42 +0800

    Update : axis click to show ray

---
+++ Assets/Scripts/VR/RayController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class RayController : MonoBehaviour
+{
+    public XRController leftRay;
+    
+    public XRController rightRay;
+    
+    public InputHelpers.Button rayActivationButton;
+
+    public float activationThreshold = 0.1f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        leftRay.gameObject.SetActive(CheckIfActivated(leftRay));
+        rightRay.gameObject.SetActive(CheckIfActivated(leftRay));
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, rayActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
