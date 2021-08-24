926b2dfd602065af7fc64731c02de42371b67fec
sangtian <sangtian0820@sjtu.edu.cn>
2021-08-24 15:25:24 +0800

    update: open/close ray

--- Assets/Scripts/VR/RayController.cs
+++ Assets/Scripts/VR/RayController.cs
@@ -12,18 +12,48 @@ public class RayController : MonoBehaviour
     
     public InputHelpers.Button rayActivationButton;
 
+    public InputHelpers.Button rayDeactivationButton;
+
+    public bool isLeftActive;
+
+    public bool isRightActive;
+
     public float activationThreshold = 0.1f;
     
     // Update is called once per frame
     void Update()
     {
-        leftRay.gameObject.SetActive(CheckIfActivated(leftRay));
-        rightRay.gameObject.SetActive(CheckIfActivated(rightRay));
+        InputHelpers.IsPressed(leftRay.inputDevice, rayActivationButton, out bool leftActivePressed, activationThreshold);
+        if(leftActivePressed)
+        {
+            isLeftActive = true;
+        }
+        InputHelpers.IsPressed(leftRay.inputDevice, rayDeactivationButton, out bool leftDeactivePressed, activationThreshold);
+        if (leftDeactivePressed)
+        {
+            isLeftActive = false;
+        }
+        InputHelpers.IsPressed(rightRay.inputDevice, rayActivationButton, out bool rightActivePressed, activationThreshold);
+        if (rightActivePressed)
+        {
+            isRightActive = true;
+        }
+        InputHelpers.IsPressed(rightRay.inputDevice, rayDeactivationButton, out bool righDeactivePressed, activationThreshold);
+        if (righDeactivePressed)
+        {
+            isRightActive = false;
+        }
+        leftRay.gameObject.SetActive(isLeftActive);
+        rightRay.gameObject.SetActive(isRightActive);
     }
 
-    public bool CheckIfActivated(XRController controller)
+    public void SetLeftInactive()
     {
-        InputHelpers.IsPressed(controller.inputDevice, rayActivationButton, out bool isActivated, activationThreshold);
-        return isActivated;
+        isLeftActive = false;
+    }
+
+    public void SetRightInactive()
+    {
+        isRightActive = false;
     }
 }
